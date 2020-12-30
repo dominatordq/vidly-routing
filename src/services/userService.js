94897f92669df32feb37dfa546507c644b86a516
dominatordq <dominatordq@gmail.com>
2020-12-30 10:28:39 -0700

    Added userService to handle registering users

---
+++ src/services/userService.js
@@ -0,0 +1,12 @@
+import http from './httpService';
+import { apiUrl } from '../config.json';
+
+const endpoint = apiUrl + "/users";
+
+export function register(user) {
+    return http.post(endpoint, {
+        email: user.username,
+        password: user.password,
+        name: user.name
+    });
+}
