2d45b88c5816de536d8aacddb49e2b2e58cc6c06
dominatordq <dominatordq@gmail.com>
2020-12-31 12:17:37 -0700

    Implemented requiring a JWT to edit/change a movie

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import logger from './logService'
+import auth from './authService';
 import { toast } from 'react-toastify';
+
+axios.defaults.headers.common['x-auth-token'] = auth.getJwt(); // if user is logged in, they can edit movies
 
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
