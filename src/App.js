7cc3c876cf1ab915adcdfc96d6d9c21b8f683232
dominatordq <dominatordq@gmail.com>
2020-12-31 11:51:27 -0700

    Refactored code to get current user based on JWT to authService

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import jwtDecode from 'jwt-decode';
-import Movies from "./components/movies";
+import Movies from './components/movies';
 import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
@@ -11,6 +10,7 @@ import NavBar from './components/navBar';
 import LoginForm from './components/loginForm';
 import Logout from './components/logout';
 import RegisterForm from './components/registerForm';
+import auth from './services/authService';
 import 'react-toastify/dist/ReactToastify.css';
 import "./App.css";
 
@@ -18,12 +18,8 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    try {
-      const jwt = localStorage.getItem("token");  // gets the JWT from local storage
-      const user = jwtDecode(jwt);  // get the current user based on their JWT
-      this.setState({ user });
-    }
-    catch (ex) {}
+    const user = auth.getCurrentUser();
+    this.setState({ user });
   }
 
   render() {
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,4 @@
+import jwtDecode from 'jwt-decode';
 import http from './httpService';
 import { apiUrl } from '../config.json';
 
@@ -12,7 +13,18 @@ export function logout() {
     localStorage.removeItem('token');   // deletes token to log the user out
 }
 
+export function getCurrentUser() {
+    try {
+        const jwt = localStorage.getItem("token");  // gets the JWT from local storage
+        return jwtDecode(jwt);  // return the current user based on their JWT
+    }
+    catch (ex) {
+        return null;
+    }
+}
+
 export default {
     login,
-    logout
+    logout,
+    getCurrentUser
 };
